dd2c3b76f87a50b662dca2d914f969c8945deb15
Marcelobernard <m4rc3l0.64@gmail.com>
2024-05-28 12:32:01 -0300

    Titulo do commit description do commit continuacao da description do commit linha 2 linha 3 linha 4 fim das linhas de teste

--- testando.js
+++ testando.js
@@ -1,3 +1,4 @@
 console.log("Testando um commit");
 console.log("Outra linha de código adicionada para testar como atualiza");
 console.log("Agora vou testar como faz o commit com a mensagem de atualização MAIS o corpo da descrição");
+console.log("Agora entendi, vou tentar fazer corretamente, o titulo será ~~> Titulo do commit | e a descrição será ~~> description do commit");
