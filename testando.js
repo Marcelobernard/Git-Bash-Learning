6b092cf9d869170fcbb3de5d7406611e2162d1fb
Marcelobernard <m4rc3l0.64@gmail.com>
2024-05-28 12:29:09 -0300

    Titulo? Corpo? o que aqui?

--- testando.js
+++ testando.js
@@ -1,2 +1,3 @@
 console.log("Testando um commit");
 console.log("Outra linha de código adicionada para testar como atualiza");
+console.log("Agora vou testar como faz o commit com a mensagem de atualização MAIS o corpo da descrição");
